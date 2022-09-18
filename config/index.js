8aaf9e348e85aa62ced597816ac97151becfeed4
JJXDT <113826403+JJXDT@users.noreply.github.com>
2022-09-18 18:27:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oXbfp54l5iGaVHylnz4cCrXWXfcc",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "6zl7QiNXlaEr7rA6KCbVFPIEIsMdM6MBZ3hzhyEqDqQ",
+      useTemplateId: "	6zl7QiNXlaEr7rA6KCbVFPIEIsMdM6MBZ3hzhyEqDqQ",
       // 所在省份
       province: "广东",
       // 所在城市
