b48a7f0638007e29117abda2bc3c636aa99d1dd7
JJXDT <113826403+JJXDT@users.noreply.github.com>
2022-09-18 18:51:46 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -355,6 +355,15 @@ export const getBirthdayMessage = (festivals) => {
         }
       }
 
+      // 节日相关
+      if (item.type === '纪念') {
+        if (item.diffDay === 0) {
+         // message = `今天已经在一起 ${ item.name } 哦，要开心！`
+        } else {
+          message = `已经在一起 ${ item.diffDay }天啦，要开心！
+        }
+      }
+      
       // 存储数据
       if (message) {
         resMessage += `${ message } \n`
